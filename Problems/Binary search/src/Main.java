267ca6f21dd2602b13d3b9445a12de3048325a64
Viktor Papara <eucalypto@mail.de>
2020-11-19 19:18:41 +0100

    Add initial files for this project

---
+++ Problems/Binary search/src/Main.java
@@ -0,0 +1,10 @@
+class BinarySearch {
+    /**
+     * @param nums ordered sequence of integers
+     * @param key  an element for searching
+     * @return index of key or a negative value
+     */
+    public static int callBinarySearch(int[] nums, int key) {
+        // write your code here
+    }
+}
